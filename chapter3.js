c88a6386e4d97c82afe247967e1910a6de8c44a9
Willson Mock <willson.mock@gmail.com>
2015-01-03 17:20:35 -0500

    Complete 3.1

--- chapter3.js
+++ chapter3.js
@@ -62,4 +62,30 @@
       return this.start >= this.end;
     }
   };
+
+  // 3.1 - Describe how to use a single array to create 3 stacks
+  /*
+   * Let's assume you have an array of size n. You can allocation the first n / 3 positions
+   * to stack1, the next n / 3 positions to stack2, and the final n / 3 positions to
+   * stack3. In addition, you'll have to store the starting positions of each of the stacks
+   * (i.e. the index within the array which will be the starting position). You'll also
+   * need to store the top positions of each of the stacks as you're 'pushing' and 'popping'
+   * values onto the stacks.
+   *
+   * In terms of implementing 'push', you'll have to specify which stack to push to. Once
+   * you know which stack to push to, you can get the top position and store the value at
+   * that position within the array before incrementing the top position for that stack and
+   * updating it.
+   *
+   * In terms of implementing 'pop', you'll have to specify which stack to pop from. Once
+   * you know which stack to pop from, you can first check if that stack is empty by comparing
+   * the top and starting positions for that stack. The stack is empty only when the top and
+   * starting positions are identical. Assuming the specific stack is not empty, you can save
+   * the value at the top position of that stack and delete the data in that position before
+   * returning the value.
+   *
+   * Implementing 'peek' will be very similar to pop - since you'll have to specify which stack
+   * to peek at, you'll have access to that stack's top position and be able to return the value
+   * at that top position.
+  */
 })();
