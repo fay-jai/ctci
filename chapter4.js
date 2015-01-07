1506715fd9163fdb48acd15a75b799a634a960a7
Willson Mock <willson.mock@gmail.com>
2015-01-07 17:16:09 -0500

    Refactor implementation of binary search tree

--- chapter4.js
+++ chapter4.js
@@ -2,7 +2,8 @@
   /*
    * Chapter 4: Trees and Graphs
   */
-  var binaryTreeNode = function (value) {
+
+  var bstNode = function (value) {
     return {
       value : value,
       left  : null,
@@ -11,105 +12,134 @@
   };
 
   var binarySearchTree = function () {
-    var tree  = Object.create( binarySearchTreeMethods );
-    tree.root = null;
-    return tree;
+    var obj  = Object.create( bstMethods );
+    obj.root = null;
+    return obj;
   };
 
-  var binarySearchTreeMethods = {
-    insert: function (value, node) {
-      var newNode = binaryTreeNode( value );
-      // check if tree is empty
+  var bstMethods = (function () {
+    var insert = function (value) {
+      var newNode = bstNode( value );
+
       if ( this.isEmpty() ) {
         this.root = newNode;
-        return;
+      } else {
+        _insert.call( this, this.root, newNode );
       }
+    };
 
-      // initialize node
-      if ( node === void 0 ) node = this.root;
-      // compare value with node.value
-      if ( value <= node.value ) {
-        // check if left exists
-        if ( node.left ) {
-          this.insert( value, node.left );
+    var _insert = function (baseNode, newNode) {
+      if ( newNode.value <= baseNode.value ) {
+        if ( baseNode.left ) {
+          _insert.call( this, baseNode.left, newNode );
         } else {
-          node.left = newNode;
+          baseNode.left = newNode;
         }
       } else {
-        if ( node.right ) {
-          this.insert( value, node.right );
+        if ( baseNode.right ) {
+          _insert.call( this, baseNode.right, newNode );
         } else {
-          node.right = newNode;
+          baseNode.right = newNode;
         }
       }
-    },
-    remove: function (value) {
-      this.root = this._removeInner( value, this.root );
-    },
-    _removeInner: function (value, node) {
+    };
+
+    var remove = function (value) {
+      if ( !this.isEmpty() ) {
+        this.root = _remove.call( this, value, this.root );
+      }
+    };
+
+    var _remove = function (value, node) {
+      var rightMin;
+
       if ( node ) {
-          if ( value < node.value ) {
-              node.left = this._removeInner( value, node.left );
-          } else if ( value > node.value ) {
-              node.right = this._removeInner( value, node.right );
-          } else if ( node.left && node.right ) {
-              node.value = this.findMinValue( node.right );
-              node.right = this._removeInner( node.value, node.right );
-          } else {
-              node = node.left || node.right;
-          }
+        if ( value < node.value ) {
+          node.left = _remove.call( this, value, node.left );
+        } else if ( value > node.value ) {
+          node.right = _remove.call( this, value, node.right );
+        } else if ( node.left && node.right ) {
+          rightMin   = findMinValue.call( this, node.right );
+          node.value = rightMin;
+          node.right = _remove.call( this, rightMin, node.right );
+        } else {
+          node = node.left || node.right;
+        }
       }
       return node;
-    },
-    contains: function (value, node) {
-      if ( this.isEmpty() ) return false;
-      // tree is not empty - initialize node
-      if ( node === void 0 ) node = this.root;
+    };
 
-      // check if node's value is the value
+    var contains = function (value) {
+      var node = Array.prototype.slice.call( arguments )[1];
+
+      if ( !this.isEmpty() ) {
+        return _contains.call( this, value, node || this.root );
+      }
+    };
+
+    var _contains = function (value, node) {
+      if ( node === null )        return false;
       if ( value === node.value ) return true;
-      if ( value < node.value ) {
-        // check if left node exists
-        return node.left ? this.contains( value, node.left ) : false;
-      } else {
-        // check if right node exists
-        return node.right ? this.contains( value, node.right ) : false;
-      }
-    },
-    findMaxValue: function (node) {
-      if ( !this.isEmpty() ) {
-        if ( node === void 0 ) node = this.root;
-        while ( node.right ) {
-          node = node.right;
-        }
-        return node.value;
-      }
-    },
-    findMinValue: function (node) {
-      if ( !this.isEmpty() ) {
-        if ( node === void 0 ) node = this.root;
-        while ( node.left ) {
-          node = node.left;
-        }
-        return node.value;
-      }
-    },
-    getHeight: function (node) {
-      if ( !this.isEmpty() ) {
-        // initialize node
-        if ( node === void 0 ) node = this.root;
+      if ( value < node.value )   return _contains.call( this, value, node.left );
+      return _contains.call( this, value, node.right );
+    };
 
-        // base case
-        if ( node.left  === null && node.right === null ) return 0;
-        if ( node.left  === null ) return 1 + this.getHeight( node.right );
-        if ( node.right === null ) return 1 + this.getHeight( node.left );
-        return 1 + Math.max( this.getHeight( node.left ), this.getHeight( node.right ) );
+    var findMinValue = function () {
+      var node = Array.prototype.slice.call( arguments )[0];
+      return _findMinValue.call( this, node || this.root );
+    };
+
+    var _findMinValue = function (node) {
+      while ( node.left ) {
+        node = node.left;
       }
-    },
-    isEmpty: function () {
+      return node.value;
+    };
+
+    var findMaxValue = function () {
+      var node = Array.prototype.slice.call( arguments )[0];
+      return _findMaxValue.call( this, node || this.root );
+    };
+
+    var _findMaxValue = function (node) {
+      while ( node.right ) {
+        node = node.right;
+      }
+      return node.value;
+    };
+
+    var getHeight = function () {
+      var node = Array.prototype.slice.call( arguments )[0];
+      if ( !this.isEmpty() ) {
+        return _getHeight.call( this, node || this.root );
+      }
+    };
+
+    var _getHeight = function (node) {
+      if ( node.left && node.right ) {
+        return 1 + Math.max( _getHeight.call( this, node.left  ),
+                             _getHeight.call( this, node.right ));
+      }
+      if ( node.left  ) return 1 + _getHeight.call( this, node.left  );
+      if ( node.right ) return 1 + _getHeight.call( this, node.right );
+      return 0;
+    };
+
+    var isEmpty = function () {
       return this.root === null;
-    }
-  };
+    };
+
+    // Public API
+    return {
+      insert       : insert,
+      remove       : remove,
+      contains     : contains,
+      findMinValue : findMinValue,
+      findMaxValue : findMinValue,
+      getHeight    : getHeight,
+      isEmpty      : isEmpty
+    };
+  })();
 
   // 4.1
   var isBalanced = function (binaryTree) {
