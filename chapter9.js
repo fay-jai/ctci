61b7b50dde08c46d9ab9e8cd1d61d8784784f3a6
Willson Mock <willson.mock@gmail.com>
2015-01-08 16:28:16 -0500

    Complete 9.1

--- chapter9.js
+++ chapter9.js
@@ -3,4 +3,23 @@
    * Chapter 9: Recursion and Dynamic Programming
   */
 
+  var waysToGoUpNsteps = (function () {
+    var cache = {};
+    return function (num) {
+      if (cache[num]) return cache[num];
+      if (num <  0 )  return 0;
+      if (num === 0)  return 1;
+
+      var minusThree,
+          minusTwo,
+          minusOne;
+
+      minusThree = waysToGoUpNsteps( num - 3 );
+      minusTwo   = waysToGoUpNsteps( num - 2 );
+      minusOne   = waysToGoUpNsteps( num - 1 );
+      cache[num] = minusThree + minusTwo + minusOne;
+
+      return cache[num];
+    };
+  })();
 })();
