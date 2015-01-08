197702ad45e827f27ae3bbf6652821ebd256601a
Willson Mock <willson.mock@gmail.com>
2015-01-08 16:51:15 -0500

    Complete 9.2 - not follow up

--- chapter9.js
+++ chapter9.js
@@ -3,6 +3,7 @@
    * Chapter 9: Recursion and Dynamic Programming
   */
 
+  // 9.1
   var waysToGoUpNsteps = (function () {
     var cache = {};
     return function (num) {
@@ -20,6 +21,19 @@
       cache[num] = minusThree + minusTwo + minusOne;
 
       return cache[num];
+    };
+  })();
+
+  // 9.2
+  var numRobotPaths = (function () {
+    var cache = {};
+    return function (x, y) {
+      if (cache['' + x + ',' + y]) return cache['' + x + ',' + y];
+      if (x  <  0 || y  <  0)      return 0;
+      if (x === 0 && y === 0)      return 1;
+
+      cache['' + x + ',' + y] = numRobotPaths(x, y - 1) + numRobotPaths(x - 1, y);
+      return cache['' + x + ',' + y];
     };
   })();
 })();
