004f5f03b1eafa39b2430afae78f624b35572dca
Willson Mock <willson.mock@gmail.com>
2015-01-09 18:23:04 -0500

    Complete 11.1

--- chapter11.js
+++ chapter11.js
@@ -4,5 +4,37 @@
   */
 
   // 11.1
-  
+  var mergeSorted = function (arrayA, arrayB) {
+    // assumption here is that both arrays are in sorted order
+    var result = [];
+    var lenA = arrayA.length;
+    var lenB = arrayB.length;
+    var i, j;
+
+    i = 0;
+    j = 0;
+
+    while (i < lenA && j < lenB) {
+      if ( arrayA[i] <= arrayB[j] ) {
+        result.push( arrayA[i] );
+        i += 1;
+      } else {
+        result.push( arrayB[j] );
+        j += 1;
+      }
+    }
+
+    // only one of the two while loops below will run
+    while (i < lenA) {
+      result.push( arrayA[i] );
+      i += 1;
+    }
+
+    while (j < lenB) {
+      result.push( arrayB[j] );
+      j += 1;
+    }
+
+    return result;
+  };
 })();
